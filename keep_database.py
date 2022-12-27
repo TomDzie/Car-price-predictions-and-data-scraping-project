193e535bc8bcb36fc381423c6aa1bfd1368b77bd
Tomasz <tomaszdzieledziak@gmail.com>
2022-12-27 18:56:45 +0100

    added stack link

--- keep_database.py
+++ keep_database.py
@@ -1,5 +1,6 @@
 with open('C:\\Users\\tom19\\VScode_Projects\\OTOmoto_project\\data\\database', 'w+', newline='') as f:
     print('pies')
+#https://stackoverflow.com/questions/26082360/python-searching-csv-and-return-entire-row
 
 # with open('database.csv', 'w', newline='') as cs_f:
 #     csv_title_list = ['Oferta od', 'Marka pojazdu', 'Price',  'Wersja', 'Model pojazdu', 'Generacja', 'Rok produkcji', 'Przebieg', 'Pojemnosc skokowa',
