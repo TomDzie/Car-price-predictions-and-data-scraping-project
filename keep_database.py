bfa968d9e905176cfa31eb1801fb076c77cb7846
Tomasz <tomaszdzieledziak@gmail.com>
2022-12-27 18:54:35 +0100

    few lines

---
+++ keep_database.py
@@ -0,0 +1,8 @@
+with open('C:\\Users\\tom19\\VScode_Projects\\OTOmoto_project\\data\\database', 'w+', newline='') as f:
+    print('pies')
+
+# with open('database.csv', 'w', newline='') as cs_f:
+#     csv_title_list = ['Oferta od', 'Marka pojazdu', 'Price',  'Wersja', 'Model pojazdu', 'Generacja', 'Rok produkcji', 'Przebieg', 'Pojemnosc skokowa',
+#                       'Rodzaj paliwa', 'Moc', 'Skrzynia biegow', 'Naped', 'Liczba drzwi', 'Kolor', 'Bezwypadkowy', 'id']
+#     writer = csv.DictWriter(cs_f, fieldnames=csv_title_list)
+#     writer.writeheader()
