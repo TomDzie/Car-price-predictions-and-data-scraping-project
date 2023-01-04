acb792ba69a63abb8c3e10932e6e890877bdb2ce
Tomasz <tomaszdzieledziak@gmail.com>
2023-01-04 22:50:45 +0100

    faster running

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import all_data_class as s
 import csv
+from datetime import date
 
-mercedes = s.Scraping('https://www.otomoto.pl/osobowe/bmw/seria-1/od-2015?search%5Bfilter_enum_generation%5D=gen-f20-2011&search%5Border%5D=filter_float_price%3Aasc', 'BMW', '&')
+today = date.today()
+model_name = 'BMW_f20_seria1'
+folder = 'BMW_f20_seria1'
+link = 'https://www.otomoto.pl/osobowe/bmw/seria-1/od-2015?search%5Bfilter_enum_generation%5D=gen-f20-2011&search%5Border%5D=filter_float_price%3Aasc'
 
-mercedes.get_href()
-model_name = ''
-day = ''
-
-with open('C:\\Users\\tom19\VScode_Projects\\OTOmoto_project\\data\\data.csv', 'w', newline='') as cs_f:
+with open(f'C:\\Users\\tom19\VScode_Projects\\OTOmoto_project\\data\\{folder}\\{model_name}_{today}.csv', 'w', newline='') as cs_f:
             csv_title_list = ['Oferta od', 'Marka pojazdu', 'Price',  'Wersja', 'Model pojazdu', 'Generacja', 'Rok produkcji', 'Przebieg', 'Pojemnosc skokowa',
                             'Rodzaj paliwa', 'Moc', 'Skrzynia biegow', 'Naped', 'Liczba drzwi', 'Kolor', 'Bezwypadkowy', 'id']
             writer = csv.DictWriter(cs_f, fieldnames=csv_title_list)
-            writer.writeheader()            
+            writer.writeheader()
+
+            mercedes = s.Scraping(f'{link}', f'{model_name}', '&')
+            mercedes.get_href()            
             for index, i in enumerate(mercedes.http_id[1]):
-                mercedes.scrap_inner_info(i, mercedes.http_id[0][index])
-
-                if mercedes.scrap_inner_info(i, mercedes.http_id[0][index]) == None:
+                a = mercedes.scrap_inner_info(i, mercedes.http_id[0][index])
+                if a == None:
                     continue
-
-                writer.writerow(mercedes.split_data(mercedes.scrap_inner_info(i, mercedes.http_id[0][index])[0], mercedes.scrap_inner_info(i, mercedes.http_id[0][index])[1])) 
+                print(f'{round((index+1)/len(mercedes.http_id[1])*100, 2)}%') # scraping progress %
+                writer.writerow(mercedes.split_data(a[0], a[1])) 
