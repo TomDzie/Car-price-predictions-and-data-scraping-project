c0d76f671f62371bc3cdf5939ba2f705cf9459a1
Tomasz <tomaszdzieledziak@gmail.com>
2023-02-17 21:36:42 +0100

    read_sql

--- data_charts.py
+++ data_charts.py
@@ -3,15 +3,22 @@ from collections import defaultdict
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sqlite3 as sq
 
-df = pd.read_csv('data\BMW_f20_seria1\database_BMW_f20_seria1.csv')
+
+con = sq.connect(f'SQLite_databases\BMW')
+cur = con.cursor()
+df = pd.read_sql("""SELECT * FROM BMW WHERE "Model pojazdu" LIKE 'Seria 5' AND "Pojemnosc skokowa" <= 3000""", con)
+
+
 df = df.loc[df['Rodzaj paliwa'] == 'Benzyna']
-df = df.loc[df['Pojemnosc skokowa'] >= 1700]
-df = df.loc[df['Pojemnosc skokowa'] <= 2500]
-# df = df.loc[df['Rok produkcji'] == 2018]
+df = df.loc[df['Price'] <= 1000000]
+df = df.loc[df['Rok produkcji'] >= 2017]
+df = df.loc[df['Rok produkcji'] <= 2020]
 df = df.loc[df['Uszkodzony'] != 'Tak']
 # df = df.loc[df['Moc'] == 115]
 
 sns.pairplot(data = df, hue = 'Rodzaj paliwa', vars= ['Moc', 'Price', 'Pojemnosc skokowa', 'Przebieg', 'Rok produkcji'])
 
 plt.show() 
+con.close()
